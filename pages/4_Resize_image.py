5839c35693de08dda93ba914830ccd813957b879
Ricardo Vieira <franciscorvpinheiro49@gmail.com>
2023-11-03 08:23:32 -0300

    Fixed image resizer

--- pages/4_Resize_image.py
+++ pages/4_Resize_image.py
@@ -10,12 +10,12 @@ st.set_page_config(
 
 st.title("Resize Image 🔎")
 
+uploaded_file = st.file_uploader("Choose an image file", type=['png', 'jpg'])
+
 width = st.number_input(f'*New width in px', value=None, placeholder="Type a number...", max_value=1460)
 height = st.number_input(f'*New height in px', value=None, placeholder="Type a number...", max_value=1460) 
 
-uploaded_file = st.file_uploader("Choose an image file", type=['png', 'jpg'])
-
-if uploaded_file is not None:
+if uploaded_file is not None and width and height:
     with Image.open(uploaded_file) as img:
         resized = img.resize((int(width), int(height)))
         st.image(resized)
